dddae195c6084cc502dd7d2825f31829dc5ac9a0
Shubham kumar <theshubham.dev@gmail.com>
2026-04-04 01:49:55 +0530

    refactor: move App component to src directory and update imports

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { AppNavigator } from './src/navigation/AppNavigator';
+import { AppNavigator } from './navigation/AppNavigator';
 
 function App() {
   return (
